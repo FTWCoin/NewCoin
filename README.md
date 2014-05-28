fc84232f280465b0302ccc8c1b175908438e6fb9
FTWCoin <difazio.andrea@live.it>
2014-05-28 11:30:23 -0700

    changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-FooCoin (FOO)
+NewCoin (FOO)
 ===========
 
-[![Build Status](https://travis-ci.org/RazorLove/foocoin.png?branch=master)](https://travis-ci.org/RazorLove/foocoin)
+[![Build Status](https://travis-ci.org/RazorLove/NewCoin.png?branch=master)](https://travis-ci.org/RazorLove/NewCoin)
 
 
 Scrypt Hashcash PoW Template
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,13 +45,13 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FooCoin");
-    case mBTC: return QString("milliFooCoin (1 / 1,000)");
-    case uBTC: return QString("microFooCoin (1 / 1,000,000)");
+    case BTC: return QString("NewCoin");
+    case mBTC: return QString("milliNewCoin (1 / 1,000)");
+    case uBTC: return QString("microNewCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
-//a single unit (.00000001) of FooCoin is called a "wander."
+//a single unit (.00000001) of NewCoin is called a "wander."
 qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
